99c7634c08f1dc7fe6ff7a7932794cd508f41fe5
Valentin Risch <valentin_risch@hotmail.de>
2016-06-27 22:19:32 +0200

    added responsive grid

---
+++ js/menu_toggle.js
@@ -0,0 +1,17 @@
+/**
+ * Created by valentin on 25/06/16.
+ */
+
+$(document).ready(function() {
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
+
+    menuToggle.on('click', function(e) {
+        e.preventDefault();
+        $('#js-navigation-menu').slideToggle(function(){
+            if($('#js-navigation-menu').is(':hidden')) {
+                $('#js-navigation-menu').removeAttr('style');
+            }
+        });
+    });
+});
